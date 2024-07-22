574b4fa2a64f25a7cf05c1a726a3c323b3b33a28
trapka <daniyar.kanu@mail.ru>
2024-07-22 11:44:05 +0500

    add other settings to d-c

--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@ from pydantic import BaseModel
 
 from faststream.kafka.fastapi import KafkaRouter, Logger
 
-router = KafkaRouter("0.0.0.0:9092")
+router = KafkaRouter("kafka:9092")
+
 
 class Incoming(BaseModel):
     m: dict
 
+
 def call():
     return True
+
 
 @router.subscriber("test")
 @router.publisher("response")
@@ -17,9 +20,11 @@ async def hello(m: Incoming, logger: Logger, d=Depends(call)):
     logger.info(m)
     return {"response": "Hello, Kafka!"}
 
+
 @router.get("/")
 async def hello_http():
     return "Hello, HTTP!"
+
 
 app = FastAPI(lifespan=router.lifespan_context)
 app.include_router(router)
